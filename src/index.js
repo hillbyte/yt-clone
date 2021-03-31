edffd9220763cad62182ef6d0fe4895d5c0fccfe
hillbyte <ahillbaky@gmail.com>
2021-03-31 23:13:37 +0530

    NULL

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
-// import "jquery/dist/jquery";
+import "jquery/dist/jquery";
 import "bootstrap/dist/js/bootstrap";
-// import "popper.js/dist/popper";
+import "popper.js/dist/popper";
 import "./index.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
