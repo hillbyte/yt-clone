20e1be4bc234640b0cb38bb612271657930e8b9a
hillbyte <ahillbaky@gmail.com>
2021-03-31 21:04:07 +0530

    remove popper

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
 import "jquery/dist/jquery";
 import "bootstrap/dist/js/bootstrap";
-import "popper.js/dist/popper";
+// import "popper.js/dist/popper";
 import "./index.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
