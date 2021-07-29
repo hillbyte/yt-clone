027351cf27ae6b61c79d18c8420d3ed0eccea0ed
hillbyte <ahillbaky@gmail.com>
2021-07-29 16:46:35 +0530

    env updated

--- src/yt.js
+++ src/yt.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-const KEY =process.env.YT_API_KEY;
+const KEY =process.env.REACT_APP_API_KEY;
+
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
   params: {
@@ -8,3 +9,5 @@ export default axios.create({
     key: KEY,
   },
 });
+
+
